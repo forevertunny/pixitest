b729a8078c05efa8f4ed79ca6f2025c2cdf79585
tunny <tunny@jetfuel.com.tw>
2019-08-05 13:21:36 +0800

    up

--- src/MVC/Mode/Proxy/GameProxy.js
+++ src/MVC/Mode/Proxy/GameProxy.js
@@ -11,9 +11,6 @@ export class GameProxy extends MVC.Proxy
         return "GameProxy";
     }
 
-
-
-
     constructor(key)
     {
         super();
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
